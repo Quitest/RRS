9ca1362dec325c668d0591994d9e27d530c14890
lenovo <none@none.org>
2021-09-14 13:05:09 +0300

    Initial commit

---
+++ src/main/java/ru/pel/ResourceReservationSystem/ResourceReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.pel.ResourceReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceReservationSystemApplication.class, args);
+	}
+
+}
