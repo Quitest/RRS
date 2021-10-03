ce42cd04cbc96cc8b581f679d67dbcd653d22d9a
Quitest <Quitest>
2021-10-03 21:09:35 +0300

    ResourceReservationSystem-20 Контроллер обработки исключений Init commit

--- src/main/java/ru/pel/ResourceReservationSystem/controllers/GlobalControllerExceptionHandler.java
+++ src/main/java/ru/pel/ResourceReservationSystem/controllers/GlobalControllerExceptionHandler.java
@@ -1,26 +1,29 @@
 package ru.pel.ResourceReservationSystem.controllers;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLException;
 
 @Controller
-public class MyErrorController /*implements ErrorController*/ {
+public class GlobalControllerExceptionHandler /*implements ErrorController*/ {
     // @RequestHandler methods
 
     // Exception handling methods
 
-//    // Convert a predefined exception to an HTTP Status code
-//    @ResponseStatus(value=HttpStatus.CONFLICT,
-//            reason="Data integrity violation")  // 409
-//    @ExceptionHandler(DataIntegrityViolationException.class)
-//    public void conflict() {
-//        // Nothing to do
-//    }
+    // Convert a predefined exception to an HTTP Status code
+    @ResponseStatus(value= HttpStatus.CONFLICT,
+            reason="Data integrity violation")  // 409
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public void conflict() {
+        // Nothing to do
+    }
 
     // Specify name of a specific view that will be used to display the error:
     @ExceptionHandler({SQLException.class, DataAccessException.class})
